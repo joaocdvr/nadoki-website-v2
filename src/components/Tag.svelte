025b52b440e97fad6b4be9d01e3c43c292e73bc5
joaocdvr <joaocdvrodrigues@gmail.com>
2020-04-16 10:51:18 +0100

    Created the Tag component that is used inside the Card component

---
+++ src/components/Tag.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let tag = "";
+</script>
+
+<style>
+  li {
+    padding: 0.75rem 0.75rem 0 0;
+  }
+
+  p {
+    background-color: var(--main-color);
+    padding: 0.375rem;
+  }
+</style>
+
+<li>
+  <p class="body-bold">{tag}</p>
+</li>
