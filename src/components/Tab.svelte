81257c1396373c8547f5ba0f6292e62a9c165c5d
joaocdvr <joaocdvrodrigues@gmail.com>
2020-04-18 09:27:45 +0100

    Switched every image from PNG to progressive JPGs

---
+++ src/components/Tab.svelte
@@ -0,0 +1,27 @@
+<style>
+  menu {
+    display: flex;
+    flex-direction: row;
+    background-color: var(--dark);
+  }
+
+  li {
+    width: 50vw;
+    text-align: center;
+    padding: 1rem 0;
+    color: var(--light);
+  }
+
+  hr {
+    height: 0.2rem;
+    border-width: 0;
+    background-color: var(--main-color);
+    margin: 0;
+  }
+</style>
+
+<menu class="body-bold">
+  <li>Hardware</li>
+  <li>Software</li>
+</menu>
+<hr noshade />
