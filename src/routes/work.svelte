d5fe7a445947660b4d86a6b760e00b25b8f7f8e1
joaocdvr <joaocdvrodrigues@gmail.com>
2020-04-18 09:39:53 +0100

    Updated the alt from the work page images

--- src/routes/work.svelte
+++ src/routes/work.svelte
@@ -12,28 +12,28 @@
   let cards = [
     {
       src: "work/above_it_all.jpg",
-      alt: "'Above It ALL' cover hurt",
+      alt: "'Above It ALL' album cover art",
       title: "ABOVE IT ALL",
       content: "Song by Mind Against.",
       tags: "Master"
     },
     {
       src: "work/parda.jpg",
-      alt: "'Parda' cover hurt",
+      alt: "'Parda' documentary cover art",
       title: "PARDA",
       content: "A Brazilian documentary, directed by Tai Linhares.",
       tags: "5.1 Mix"
     },
     {
       src: "work/love_hurts.jpg",
-      alt: "'Love Hurts' cover art",
+      alt: "'Love Hurts' single cover art",
       title: "LOVE HURTS",
       content: "Song by Osaka88.",
       tags: "Mix, Master"
     },
     {
       src: "work/der_hauptbanhof.jpg",
-      alt: "'Der Hauptbanhof' cover art",
+      alt: "'Der Hauptbanhof' documentary cover art",
       title: "DER HAUPTBANHOF",
       content:
         "A documentary written, directed and filmed by Jordi Garcia Rodriguez.",
@@ -41,7 +41,7 @@
     },
     {
       src: "work/ertrinken.jpg",
-      alt: "Frame from 'Ertrinken'",
+      alt: "Frame from 'Ertrinken' short film",
       title: "ERTRINKEN",
       content:
         "A short story directed by Pedro Harres and produced by Cléo Campe.",
@@ -49,7 +49,7 @@
     },
     {
       src: "work/a_juventude_por_ela_própria.jpg",
-      alt: "Frame from 'A juventude por ela própria'",
+      alt: "Frame from 'A juventude por ela própria' documentary",
       title: "A JUVENTUDE POR ELA PRÓPRIA",
       content:
         "A documentary produced by Agora, irrepetível. and edited by Guilherme Sousa.",
