457ab2f28b831b9fb1c33d7b06dc5e88538aae65
joaocdvr <joaocdvrodrigues@gmail.com>
2020-04-21 15:45:17 +0100

    Added content to every submenu from the services page

--- src/routes/services.svelte
+++ src/routes/services.svelte
@@ -103,27 +103,37 @@
     {
       title: "Re-recording mixing",
       function: handleFilmFirstSubmenuClick,
-      variable: $isFilmFirstClicked
+      variable: $isFilmFirstClicked,
+      content:
+        "Nadoki offers to mix the sound for your production balancing all dialog, ADR, sound effects, music, atmospheres and foleys so that it enhances the work’s storytelling without distracting from it.<br><br>We offer both stereo and 5.1 surround mixing. For other formats, please get in touch."
     },
     {
       title: "Editing",
       function: handleFilmSecondSubmenuClick,
-      variable: $isFilmSecondClicked
+      variable: $isFilmSecondClicked,
+      content:
+        "Nadoki offers dialogue, music and sound effect editing for your production."
     },
     {
       title: "Restoration",
       function: handleFilmThirdSubmenuClick,
-      variable: $isFilmThirdClicked
+      variable: $isFilmThirdClicked,
+      content:
+        "Nadoki offers audio restoration services to clean, fix and restore your audio recordings. We can remove pops, clicks, hum, hiss, crackle, and many other noises, as well as remove clipping of an audio file.<br><br>Please make sure your files are according to our requirements."
     },
     {
       title: "Sound Design",
       function: handleFilmFourthSubmenuClick,
-      variable: $isFilmFourthClicked
+      variable: $isFilmFourthClicked,
+      content:
+        "Sound has an important role in storytelling in a film production - it allows the creation of atmospheres, emotion and it sets the tone of a movie.<br><br>Nadoki offers to sound design your film production to enhance your story to its full potential. Using high-quality industry standard samples and foley recording techniques, we can create the sound your production needs."
     },
     {
       title: "Original Soundtrack",
       function: handleFilmFifthSubmenuClick,
-      variable: $isFilmFifthClicked
+      variable: $isFilmFifthClicked,
+      content:
+        "Nadoki can compose a tailored music score to accompany your film production. Using our hybrid setup, we have in-the-box tools, as well as, analog instruments to create the sound that fits your production."
     }
   ];
 
@@ -131,22 +141,30 @@
     {
       title: "Sound Design",
       function: handleGameFirstSubmenuClick,
-      variable: $isGameFirstClicked
+      variable: $isGameFirstClicked,
+      content:
+        "The multiplicity and variety of possibilities in sound composition in a game not only allow the creation of atmospheres and emotions, but also sets the tone of it.<br><br>Nadoki offers to sound design your game to enhance your story to its full potential. Using high-quality industry-standard samples and foley recording techniques, we can create the sound your production needs."
     },
     {
       title: "Original Soundtrack",
       function: handleGameSecondSubmenuClick,
-      variable: $isGameSecondClicked
+      variable: $isGameSecondClicked,
+      content:
+        "Nadoki offers to compose tailored music scores to accompany your game. With the unlimited possibilities of the in-the-box approach, together with the analog instruments available in our array of sounds, we can translate your game’s message into music."
     },
     {
       title: "Integration",
       function: handleGameThirdSubmenuClick,
-      variable: $isGameThirdClicked
+      variable: $isGameThirdClicked,
+      content:
+        "The accuracy of sound integration to the environment of a game has an important role in the immersion of the player. For the finest interaction, precision in each moment of scene is crucial and making sure that all audio assets (music, ambience, dialogue, and sound effects) load smoothly, trigger correctly, and playback in the highest quality at minimal processing power is key.<br><br>Nadoki offers to integrate the sound of your game utilizing FMOD, Unreal Engine and Unity."
     },
     {
       title: "Mixing",
       function: handleGameFourthSubmenuClick,
-      variable: $isGameFourthClicked
+      variable: $isGameFourthClicked,
+      content:
+        "Nadoki offers mixing services for the sound of your game balancing all dialog, ADR, sound effects, music, atmospheres and foleys for the best immersion of the player.<br><br>We offer both stereo and 5.1 surround mixing. For other formats, please get in touch."
     }
   ];
 
@@ -154,17 +172,23 @@
     {
       title: "Sonic Logo",
       function: handleBrandingFirstSubmenuClick,
-      variable: $isBrandingFirstClicked
+      variable: $isBrandingFirstClicked,
+      content:
+        "There’s more to branding than what meets the eye. While strong visuals are key, it’s also important to communicate your story and create your sonic identity for your brand.<br><br>After a talk to understand the story and personality of your brand, we will analyze to find your voice, and create a sonic logo customized for your company, to best translate your energy, message, and experience to your clients."
     },
     {
       title: "Podcast",
       function: handleBrandingSecondSubmenuClick,
-      variable: $isBrandingSecondClicked
+      variable: $isBrandingSecondClicked,
+      content:
+        "The fastest-growing media content for brands is used nowadays by several businesses to best connect with their customers at a personal level. Podcasts can be a key tool in the digital marketing strategy of your company to keep your clients connected to your brand and increase the reach and your participation with your community.<br><br>We offer podcast directing, recording, editing and finalization for your business."
     },
     {
       title: "Post-production for Ads",
       function: handleBrandingThirdSubmenuClick,
-      variable: $isBrandingThirdClicked
+      variable: $isBrandingThirdClicked,
+      content:
+        "Advertisements are around us every day. From radio spots to TV spots and now online streaming spots, it’s the most common advertising media.<br><br>Nadoki offers audio post-production for your advertisements. Our services include ADR recording, editing, restoration, sound designing, mixing, and finalizing. We can make your ad ready to be heard anywhere."
     }
   ];
 </script>
@@ -189,12 +213,39 @@
 {/if}
 {#if $isFilmClicked}
   <Tab tab={filmTab} />
+  {#if $isFilmFirstClicked}
+    <TextBlock content={filmTab[0].content} />
+  {:else if $isFilmSecondClicked}
+    <TextBlock content={filmTab[1].content} />
+  {:else if $isFilmThirdClicked}
+    <TextBlock content={filmTab[2].content} />
+  {:else if $isFilmFourthClicked}
+    <TextBlock content={filmTab[3].content} />
+  {:else if $isFilmFifthClicked}
+    <TextBlock content={filmTab[4].content} />
+  {/if}
 {/if}
 {#if $isGameClicked}
   <Tab tab={gameTab} />
+  {#if $isGameFirstClicked}
+    <TextBlock content={gameTab[0].content} />
+  {:else if $isGameSecondClicked}
+    <TextBlock content={gameTab[1].content} />
+  {:else if $isGameThirdClicked}
+    <TextBlock content={gameTab[2].content} />
+  {:else if $isGameFourthClicked}
+    <TextBlock content={gameTab[3].content} />
+  {/if}
 {/if}
 {#if $isBrandingClicked}
   <Tab tab={brandingTab} />
+  {#if $isBrandingFirstClicked}
+    <TextBlock content={brandingTab[0].content} />
+  {:else if $isBrandingSecondClicked}
+    <TextBlock content={brandingTab[1].content} />
+  {:else if $isBrandingThirdClicked}
+    <TextBlock content={brandingTab[2].content} />
+  {/if}
 {/if}
 {#if !$isAnyServiceClicked}
   <Footer />
