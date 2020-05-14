c70274ff6f4375b0690cba026b2ef462d3ef810c
joaocdvr <joaocdvrodrigues@gmail.com>
2020-05-14 10:33:24 +0100

    Updated the EquipmentContent container's lists

--- src/containers/EquipmentContent.svelte
+++ src/containers/EquipmentContent.svelte
@@ -25,96 +25,45 @@
   ];
 
   let hardwareList = [
-    { open: false, name: "Computer", content: "— Mac Pro 5.1" },
     {
       open: false,
-      name: "Audio Interface",
-      content: "— UAD Apollo 8 Quad Silver"
-    },
-    {
-      open: false,
-      name: "Speakers",
+      name: "Monitoring System",
       content:
-        "— ADAM S3V (pair)<br>— Neumann KH 120 A (pair)<br>— Avantone Mixcube"
-    },
-    {
-      open: false,
-      name: "Monitor Controller",
-      content: "— SPL Model 2381"
+        "Speakers<br>— ADAM S3V (pair)<br>— Neumann KH 120 A (pair)<br>— Avantone Mixcube (mono)<br><br>Heaphones<br>— Beyerdynamic DT 770 Pro<br>— Beyerdynamic DT 880 Pro<br>— Sony MDR-7506<br>— AKG K 272 HD<br><br>Monitor Controller<br>— SPL Model 2381"
     },
     {
       open: false,
       name: "Microphones",
       content:
-        "— Neumann TLM 103<br>— Neumann KM 184 (pair)<br>— AKG C414 XLS<br>— Bluebird<br>— Shure SM57<br>— Yeti Blue Digital Mic"
+        "Dynamic<br>— Shure SM57<br>— SM7<br>— AKG P4 (x4)<br>— AKG P2<br><br>Condenser<br>— AKG C414 XLS<br>— Neumann TLM 103 Studio Set<br>— Neumann KM 184 (pair)<br>— Bluebird<br>— Yeti Blue Digital Mic<br>— AKG P17 (pair)"
     },
     {
       open: false,
-      name: "Compressors",
-      content: "— AnaMod 670 Stereo Compressor<br>— EL8 Distressor"
-    },
-    {
-      open: false,
-      name: "Preamps",
-      content: "— Apollo preamps [x4]<br>— Scarlett OctoPre preamps [x8]"
-    },
-    {
-      open: false,
-      name: "Spectrum Analyzer",
-      content: "— Audioscope Model 2813-E"
-    },
-    {
-      open: false,
-      name: "Control Surface",
-      content: "— Avid Artist Mix Controller"
-    },
-    {
-      open: false,
-      name: "MIDI Keyboard",
-      content: "— AKAI MPK49"
-    },
-    {
-      open: false,
-      name: "Headphones",
+      name: "Ourboard",
       content:
-        "— Beyerdynamic DT 770 Pro<br>— Beyerdynamic DT 880 Pro<br>— Sony MDR-7506<br>— AKG K 272 HD"
+        "Dynamics<br>— EL8 Distressor<br>— AnaMod 670 Stereo Compressor<br>— SSL Fusion<br><br>EQ<br>— Dangerous BAX EQ<br><br>Preamps<br>— Neve 1073SPX<br>— Apollo unison-enabled preamp (x4)<br>— Scarlett preamp (x8)<br><br>DI<br>— Radial Engineering J48 Active (Mono)<br><br>Converters<br>— Universal Audio Apollo Quad (silver)<br>— Focusrite Scarlett 18i20<br><br>Miscellaneous<br>— Audioscope Model 2813-E<br>— Radial Engineering X-Amp Active (dual output)<br>— Apogee BigBen Master Clock<br>— TC Electronic Clarity M Stereo"
     },
     {
       open: false,
-      name: "Amps",
+      name: "Amplifier",
       content: "— Vox AC15H<br>— Vox V212C"
-    },
-    {
-      open: false,
-      name: "DI Box",
-      content: "— Radial Engineering J48 Active (Mono)"
-    },
-    {
-      open: false,
-      name: "Reamp Box",
-      content: "— Radial Engineering X-Amp Active (Dual Output)"
     },
     {
       open: false,
       name: "Pedals",
       content:
-        "— Boss RC-30 Dual Track Looper<br>— Boss TR-2 Tremolo<br>— Boss BD-2 Blues Driver<br>— TC Electronic Flashback Delay and Looper<br>— Line6 DL4 Delay Modeller<br>— Ibanez TS9 Tube Screamer<br>— Dunlop Crybaby GCB95 Wah-Wah"
+        "Time based<br>— Boss RC-30 Dual Track Looper<br>— TC Electronic Flashback Delay and Looper<br>— Line6 DL4 Delay Modeller<br><br>Distortion<br>— Ibanez TS9 Tube Screamer<br>— Boss TR-2 Tremolo<br>— Dunlop Crybaby GCB95 Wah-Wah<br>— Boss BD-2 Blues Driver"
     },
     {
       open: false,
-      name: "Synthesizer",
-      content: "— Arturia MicroBrute"
-    },
-    {
-      open: false,
-      name: "Step Sequencer",
-      content: "— KORG SQ-1"
-    },
-    {
-      open: false,
-      name: "Modular",
+      name: "Instruments",
       content:
-        "— A-110-1 standard VCO (Doepfer)<br>— Mult (Make Noise)<br>— Braids (Mutable Instruments)<br>— A-124 wasp filter (Doepfer)<br>— A-130 Linear VCA (Doepfer)<br>— A-132-3 dual linear/exponential VCA (Doepfer)<br>— A-180-2 Multiples (Doepfer)<br>— A-140-1 ADSR Envelope Generator [x3] (Doepfer)<br>— A-180-1 Multiples (Doepfer)<br>— A-138 Mixer (Doepfer)<br>— R-51 Vacuum Tube VCA/Distortion (Metasonix)<br>— Plasma Drive (Erica Synths)<br>— R-54 Supermodule VCO/VCF (Metasonix)<br>— Wogglebug (Make Noise)<br>— MIDI to Trigger module (Erica Synths)<br>— Audio I/O (Intellijel)"
+        "Stringed Instrument<br>— Gibson SG Guitar<br>— Fender Telecaster Vintage 52’ Guitar<br>— Fender Stratocaster 1988<br>— Little Martin Acoustic Guitar<br>— Epiphone MM-50E Professional Mandolin<br>— Ukulele<br><br>Keys<br>— Arturia MicroBrute<br>— AKAI MPK49<br><br>Step Sequencer<br>— KORG SQ-1<br><br>Modular<br>— A-110-1 standard VCO (Doepfer)<br>— Mult (Make Noise)<br>— Braids (Mutable Instruments)<br>— A-124 wasp filter (Doepfer)<br>— A-130 Linear VCA (Doepfer)<br>— A-132-3 dual linear/exponential VCA (Doepfer)<br>— A-180-2 Multiples (Doepfer)<br>— 3x A-140-1 ADSR Envelope Generator (Doepfer)<br>— A-180-1 Multiples (Doepfer)<br>— A-138 Mixer (Doepfer)<br>— R-51 Vacuum Tube VCA/Distortion (Metasonix)<br>— Plasma Drive (Erica Synths)<br>— R-54 Supermodule VCO/VCF (Metasonix)<br>— Wogglebug (Make Noise)<br>— MIDI to Trigger module (Erica Synths)<br>— Audio I/O (Intellijel)"
+    },
+    {
+      open: false,
+      name: "Controller",
+      content: "— Avid Artist Mix Controller"
     }
   ];
 
@@ -141,7 +90,7 @@
       open: false,
       name: "Plug-ins",
       content:
-        "— Antares: Auto-Key | Auto-Tune Pro<br>— Cableguys: Half-Time | TimeShaper<br>— Camel Audio: CamelCrusher<br>— Celemony: Melodyne 4 Studio<br>— Fabfilter: Pro-Q 2<br>— Illformed: Glitch 2<br>— iZotope: Music Production Suite<br>— Lexicon: LXPChamber | LXPHall | LXPPlate | LXRRoom<br>— Native Instruments: Komplete 12 Ultimate<br>— oeksound: soothe<br>— Process.Audio: Sugar<br>— Plugin Alliance: bx_cleansweep V2 | bx_rockrack V3 Player | bx_solo | bx_subfilter | dearVR Pro | elysia niveau filter | Schoeps Double MS | SPL Free Ranger<br>— Slate Digital: All Access Pass<br>— Softube: Saturation Knob<br>— Sonarworks: Reference 4<br>— Sonnox: Oxford Dynamic EQ | Oxford Inflator | Oxford Limiter<br>— Soundtoys: Soundtoys 5<br>— Valhalla DSP: ValhallaVintageVerb<br>— Waves: Aphex Vintage Exciter | API 550 | Butch Vig Vocals | Chris Lord-Alge Signature | dbx 160 Compressor/Limiter | Horizon | J37 Tape | Linear Phase Multiband Compressor | Nx | SSL G-Equalizer/Master Buss Compressor | StudioRack | Waves Tune Real-Time<br>— Xfer Records: Serum | Cthulhu"
+        "Antares<br>— Auto-Key<br>— Auto-Tune Pro<br><br>Cableguys<br>— Half-Time<br>— TimeShaper<br><br>Camel Audio<br>— CamelCrusher<br><br>Celemony<br>— Melodyne 4 Studio<br><br>Fabfilter<br>— Pro-Q 2<br>— Pro-Q 3<br>— Pro-L 2<br>— Pro-R<br>— Pro-C 2<br>— Pro-MB<br>— Pro-DS<br>— Pro-G<br><br>Illformed<br>— Glitch 2<br><br>iZotope<br>— Music Production Suite<br><br>Lexicon<br>— LXPChamber<br>— LXPHall<br>— LXPPlate<br>— LXRRoom<br><br>Native Instruments<br>— Komplete 12 Ultimate<br><br>oeksound<br>— soothe<br><br>Process.Audio<br>— Sugar<br><br>Plugin Alliance<br>— bx_cleansweep V2<br>— bx_rockrack V3 Player<br>— bx_solo<br>— bx_subfilter<br>— dearVR Pro<br>— elysia niveau filter<br>— Schoeps Double MS<br>— SPL Free Ranger<br><br>Slate Digital<br>— All Access Pass<br><br>Softube<br>— Saturation Knob<br><br>Sonarworks<br>— Reference 4<br><br>Sonnox<br>— Oxford Dynamic EQ<br>— Oxford Inflator<br>— Oxford Limiter<br><br>Soundtoys<br>— Soundtoys 5<br><br>Valhalla DSP<br>— ValhallaVintageVerb<br><br>Waves<br>— Aphex Vintage Exciter<br>— API 550<br>— Butch Vig Vocals<br>— Chris Lord-Alge Signature<br>— dbx 160 Compressor/Limiter<br>— Horizon<br>— J37 Tape<br>— Linear Phase Multiband Compressor<br>— Nx<br>— SSL G-Equalizer/Master Buss Compressor<br>— StudioRack<br>— Waves Tune Real-Time<br><br>Xfer Records<br>— Serum<br>— Cthulhu"
     }
   ];
 
