1ba484a5c4b7a9fa4c163de1212b562484af1590
joaocdvr <joaocdvrodrigues@gmail.com>
2020-05-23 19:43:38 +0100

    Added a wrapper-div with margin-top to the index page to offset the content by the same height of the NavBar

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -17,11 +17,12 @@
   });
 </script>
 
+<style>
+  div {
+    margin-top: 4.5rem;
+  }
+</style>
+
 <div style="transform: translateY({$scrollYPosition * -1}px)">
   <Footer />
-  <p class="body-regular">body-regular 1rem</p>
-  <p class="body-bold">body-bold 1rem</p>
-  <p class="body-extra">body-extra 1rem</p>
-  <p class="body-italic">body-italic 1rem</p>
-  <p class="body-mono">body-mono 1rem</p>
 </div>
