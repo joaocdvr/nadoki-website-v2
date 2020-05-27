0da7e59ce7e4f8a8b608e8ea8091f879fb5f6622
joaocdvr <joaocdvrodrigues@gmail.com>
2020-05-27 17:44:51 +0100

    Updated the List component to match the redesign of the other pages

--- src/components/List.svelte
+++ src/components/List.svelte
@@ -16,10 +16,10 @@
   button {
     border: none;
     cursor: pointer;
+    position: relative;
     display: flex;
     justify-content: space-between;
-    width: calc(100vw - 3rem);
-    transition: background-color 300ms ease-in-out;
+    width: 100%;
   }
 
   button:focus {
@@ -30,29 +30,63 @@
     background-color: var(--secondary-color);
   }
 
-  :global(.user-is-tabbing) button:focus p,
   :global(.user-is-tabbing) button:focus dt {
     color: var(--light);
   }
 
   dl {
-    border-bottom: 0.0625rem solid var(--main-color);
+    border-bottom: 0.125rem solid var(--dark);
+    background-color: var(--light);
+    transition: background-color 300ms ease-in-out;
+  }
+
+  dt {
+    padding: 1.5rem 0 1.5rem 1.5rem;
   }
 
   dd {
-    padding-bottom: 0.75rem;
+    padding: 0 1.5rem 1.5rem 1.5rem;
+  }
+
+  .item-is-clicked {
+    background-color: var(--main-color);
+  }
+
+  .item-is-clicked button:before {
+    transform: rotate(-90deg);
+  }
+
+  button:after {
+    content: "";
+    position: absolute;
+    top: 2.125rem;
+    right: 1.5rem;
+    width: 1rem;
+    height: 0.25rem;
+    background-color: var(--dark);
+  }
+
+  button:before {
+    content: "";
+    position: absolute;
+    top: 1.75rem;
+    right: 1.875rem;
+    width: 0.25rem;
+    height: 1rem;
+    background-color: var(--dark);
+    transition: transform 300ms;
   }
 </style>
 
 <dl
-  in:fade={{ delay: $animationInDelay + i * 100, duration: $animationInDuration, easing: $animationInEasing }}
+  class:item-is-clicked={listItem.open}
+  in:fade={{ delay: $animationInDelay + i * 50, duration: $animationInDuration, easing: $animationInEasing }}
   out:fade={{ duration: $animationOutDuration, easing: $animationOutEasing }}>
   <button
     on:click
     aria-label="Toggle {listItem.name} list"
     aria-pressed={listItem.open}>
-    <dt class="list-name">{listItem.name}</dt>
-    <p class="list-name">{listItem.open ? '-' : '+'}</p>
+    <dt class="body-bold">{listItem.name}</dt>
   </button>
 
   {#if listItem.open}
