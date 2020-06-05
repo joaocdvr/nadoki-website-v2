8a0cb62edfe598241041fd9e0c7cc8b942e34d2c
joaocdvr <joaocdvrodrigues@gmail.com>
2020-06-05 15:26:21 +0100

    Fixed Card's studio version media query

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -74,6 +74,14 @@
     padding: 3rem 3rem 7rem 3rem;
   }
 
+  @media (--max-content-width) {
+    .studio-card-wrapper {
+      width: var(--max-width);
+      transform: translateX(calc(50vw - var(--max-width) * 0.5));
+      padding: 3rem 1.5rem 7rem 1.5rem;
+    }
+  }
+
   img {
     width: 100%;
     transition: filter 300ms ease-in-out;
