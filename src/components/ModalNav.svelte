f0ebc0cbe0f75a7a656885898205c4e55db4f029
joaocdvr <joaocdvrodrigues@gmail.com>
2020-06-13 18:20:06 +0100

    Changed the position of the back button from ModalNav to match the homepage button position

--- src/components/ModalNav.svelte
+++ src/components/ModalNav.svelte
@@ -40,11 +40,22 @@
     top: 0;
     left: 0;
     width: 100vw;
+    background-color: var(--light);
+    z-index: 1;
+    border-bottom: 0.125rem solid var(--dark);
+  }
+
+  div {
+    width: 100%;
+    max-width: var(--max-width);
     display: flex;
     justify-content: space-between;
-    background-color: var(--light);
-    z-index: 3;
-    border-bottom: 0.125rem solid var(--dark);
+  }
+
+  @media (--max-content-width) {
+    div {
+      transform: translateX(calc(50vw - var(--max-width) / 2));
+    }
   }
 
   h1 {
@@ -66,23 +77,25 @@
 </style>
 
 <nav>
-  <button on:click aria-label="Back">
-    <svg
-      viewBox="0 0 40 24"
-      preserveAspectRatio="none"
-      xmlns="http://www.w3.org/2000/svg">
-      <path
-        d="M22.3783 3.021H10V0L0 5.39L10 11V8H22.3783C27.4967 8 31.6467 10.356
-        31.6467 13.427C31.6467 16.498 27.4983 19 22.3783
-        19H10V24H22.3783C32.1117 24 40 19.266 40 13.427C40 7.588 32.1117 3.021
-        22.3783 3.021Z" />
-    </svg>
-  </button>
-  <h1
-    class="body-bold"
-    in:fade={{ delay: $animationInDelay, duration: $animationInDuration, easing: $animationInEasing }}
-    out:fade={{ delay: $animationInDelay, duration: $animationOutDuration, easing: $animationOutEasing }}>
-    {title}
-  </h1>
-  <span />
+  <div>
+    <button on:click aria-label="Back">
+      <svg
+        viewBox="0 0 40 24"
+        preserveAspectRatio="none"
+        xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M22.3783 3.021H10V0L0 5.39L10 11V8H22.3783C27.4967 8 31.6467 10.356
+          31.6467 13.427C31.6467 16.498 27.4983 19 22.3783
+          19H10V24H22.3783C32.1117 24 40 19.266 40 13.427C40 7.588 32.1117 3.021
+          22.3783 3.021Z" />
+      </svg>
+    </button>
+    <h1
+      class="body-bold"
+      in:fade={{ delay: $animationInDelay, duration: $animationInDuration, easing: $animationInEasing }}
+      out:fade={{ delay: $animationInDelay, duration: $animationOutDuration, easing: $animationOutEasing }}>
+      {title}
+    </h1>
+    <span />
+  </div>
 </nav>
