c31e79891239e7a5d5888511dcf6715c332c4bc0
joaocdvr <joaocdvrodrigues@gmail.com>
2020-06-15 13:03:52 +0100

    Added revisioned text to studio page

--- src/containers/StudioContent.svelte
+++ src/containers/StudioContent.svelte
@@ -11,7 +11,7 @@
       alt: "Nadoki Studios picture nº1",
       title: "Nadoki",
       content:
-        "We are a mixing and mastering studio located at Riverside Studios. Our studio was founded in 2019 after four educated engineers united to bring a fresh new start to the industry. With our hybrid analog + digital approach, we have the tools and ears to make your idea sound as it deserves.<br><br>Our control room is equipped with multiple pairs of speakers, outboard gear, instruments and synthesizers. Our mixing and mastering suite is also a perfect option for producing and writing sessions.",
+        "We are a mixing and mastering studio located at Riverside Studios. Our studio was founded in 2019 after four educated engineers united to bring a fresh new start to the industry. With our hybrid analog + digital approach, we have the tools and ears to make your idea sound as it deserves.<br><br>Our control room is equipped with multiple pairs of speakers, outboard gear, instruments, and synthesizers. Our mixing and mastering suite is also a perfect option for producing and writing sessions.",
       tags: ""
     },
     {
@@ -20,7 +20,7 @@
       alt: "The Lab picture nº1",
       title: "The Lab",
       content:
-        "The Riverside eventspace is a flexible live tracking room that is perfect for a full band, orchestra, or choir recording",
+        "The Riverside eventspace is a flexible live tracking room that is perfect for a full band, orchestra, or choir recording.",
       tags: ""
     },
     {
@@ -29,7 +29,7 @@
       alt: "Studio 10 picture nº1",
       title: "Studio 10",
       content:
-        "Studio 10 is our tracking booth for vocals and overdubs recordings. It is the perfect space for smaller-scaled projects.",
+        "Studio 10 is our tracking booth for vocals and overdub recordings. It is the perfect space for smaller-scaled projects.",
       tags: ""
     },
 
@@ -39,7 +39,7 @@
       alt: "Riverside Studios picture nº1",
       title: "Riverside Studios",
       content:
-        "The complex we are located at is a unique collective of renowned artists and professionals located in Berlin’s Media Spree, right by the water, neighboring most of the industry’s leaders. ",
+        "We are part of the Riverside Studio complex, a unique collective of renowned artists and professionals located in Berlin’s Media Spree, right by the water, neighboring most of the industry’s leaders.",
       tags: "",
       link: true,
       linkHref: "https://riversidestudios.de/"
